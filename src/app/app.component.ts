4f9f7c8105685434ec32e9067c39db3c161bf6f8
yogendra jadhav <yj.withyou@gmail.com>
2025-05-31 17:24:19 +0530

    modified to add loading spinner logic and link added for admin and profile page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationCancel, NavigationEnd, NavigationStart, NavigationError, Event, RouterModule, RouterOutlet } from '@angular/router';
+import { LoadingService } from './services/loading.service';
+import { BehaviorSubject } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'AngularStandaloneAppWithRouting';
+  loading$: BehaviorSubject<boolean>;
+
+  constructor(private loadingService: LoadingService, private router: Router) {
+    // Create a BehaviorSubject to hold the loading state
+    // Initialize loading$ with the loading service's loading BehaviorSubject
+    this.loading$ = this.loadingService.loading;
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      // Check if the event is a NavigationStart event
+      if (event instanceof NavigationStart) {
+        // Show the loading indicator when navigation starts
+        this.loadingService.show();
+      }
+      else if (event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError) {
+        // Hide the loading indicator when navigation ends, is cancelled, or encounters an error
+        this.loadingService.hide();
+      }
+    });
+  }
+
 }
