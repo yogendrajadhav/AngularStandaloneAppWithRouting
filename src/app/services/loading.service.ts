82eebb87ee60f79ee672f5d7f860a529d3ed617f
yogendra jadhav <yj.withyou@gmail.com>
2025-05-31 17:22:23 +0530

    loading service created.

---
+++ src/app/services/loading.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+loading =new BehaviorSubject<boolean>(false);
+  constructor() { }
+  show(){
+    this.loading.next(true);
+  }
+  hide(){
+    this.loading.next(false)
+  }
+}
