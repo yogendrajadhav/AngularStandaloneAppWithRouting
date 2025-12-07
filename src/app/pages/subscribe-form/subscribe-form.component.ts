73c93b1c3647e1308002450be696d840f3a9c2f8
yogendra jadhav <yj.withyou@gmail.com>
2025-12-07 13:39:08 +0530

    email and yearsAsFan fields validation implemented

--- src/app/pages/subscribe-form/subscribe-form.component.ts
+++ src/app/pages/subscribe-form/subscribe-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, effect, signal } from '@angular/core';
-import { email, Field, form, required, schema } from '@angular/forms/signals';
-import { max } from 'rxjs';
+import { email, Field, form, max, min, minLength, required, schema } from '@angular/forms/signals';
 @Component({
   selector: 'app-subscribe-form',
   standalone: true,
@@ -14,7 +13,7 @@ throw new Error('Method not implemented.');
 }
 
   subscribeModel=signal<SubscriptionData>(initialData);
-  subscribeForm=form(this.subscribeModel);
+  subscribeForm=form(this.subscribeModel,subscriptionSchema);
   eff=effect(()=>{
     console.log(`Email: ${this.subscribeModel().email}`);
   });
@@ -35,10 +34,10 @@ export const initialData: SubscriptionData = {
   yearsAsFan: NaN
 };
 
-export const SubscriptionSchema=schema<SubscriptionData>((rootPath)=>{
+export const subscriptionSchema=schema<SubscriptionData>((rootPath)=>{
    required(rootPath.email,{message:'Email Address is required'});
    email(rootPath.email,{message:'Enter a valid email address'});
+   minLength(rootPath.email,6,{message:'Email must be at least 6 characters long'});
+   min(rootPath.yearsAsFan,0,{message:'Years cannot be negative'});
+   max(rootPath.yearsAsFan,100,{message:'please enter a valid number of years'});
 });
- 
-
-
